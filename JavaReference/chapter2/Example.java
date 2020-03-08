42e10d495113679331078d3f7e4ae365288cf761
olaez <olaez@DESKTOP-LIP6K8G>
2020-03-07 22:54:28 -0600

    Add chapter2

---
+++ JavaReference/chapter2/Example.java
@@ -0,0 +1,13 @@
+/*This is a simple Java program
+ * call this file Example.java
+ * */
+public class Example 
+{
+	//Your program begins with a call to main
+	public static void main(String[] args)
+	{
+		System.out.println("This is a simple Java program.");
+
+	}
+
+}
